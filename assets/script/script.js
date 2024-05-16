e18ad793911b55ac029ba3188929461f730cdc59
cooosmo <cosimo-marasciulo@libero.it>
2024-05-16 18:56:52 +0200

    modifiche header e js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,31 @@
 const header = document.querySelector('.big-searchbar')
 
-const homeHeader = document.querySelector('.pagina-1 header')
-
 window.addEventListener('scroll', ()=>{
 
     if(scrollY > 0) {
         header.classList.add('hide-me')
-        homeHeader.classList.add('bg-white')
+        body.classList.add('scrolled')
     } else {
         header.classList.remove('hide-me')
-        homeHeader.classList.remove('bg-white')
+        body.classList.remove('scrolled')
     }
     
 })
+
+
+const body = document.querySelector('body')
+
+if(body.classList.contains('pagina-1')) {
+    const homeHeader = document.querySelector('.pagina-1 header')
+
+    window.addEventListener('scroll', ()=>{
+
+        if(scrollY > 0) {
+            homeHeader.classList.add('bg-white')
+        } else {
+            homeHeader.classList.remove('bg-white')
+        }
+        
+    })
+}
+
